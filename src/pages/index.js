20ef9805d0614089ef723f2dd0238740a2649afc
santtuss <SanttuAmi.Saaranen@metropolia.fi>
2024-09-08 14:23:11 +0300

    added the About component to the index page, modified it a little

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import About from '../components/About'
 
 function index() {
     return (
         <div className="w-full min-h-[90vh]">
-            {/* remove this comment + replace the <p> with your code */}
-            <p className="p-[20px]">hello from index.js</p>
+            <About />
         </div>
     )
 }
