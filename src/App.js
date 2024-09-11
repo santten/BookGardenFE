43d47e0cc6a62ff13840f2318a9340a5e773cf87
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-09-11 23:06:34 +0300

    Update useraccount

--- src/App.js
+++ src/App.js
@@ -1,33 +1,73 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from './components/NavBar';
-import Home from './pages/index';
-import Store from './pages/store';
-import Contacts from './pages/contacts';
-import Search from './pages/search';
-import Cart from './pages/cart';
-import Login from './pages/login';
-import Footer from './components/Footer';
-
-import './App.css';
-
+// App.js
+import React from 'react';
+import MyAccount from './components/MyAccount';
+import AccountDetails from './components/AccountDetails';
 
 function App() {
   return (
-    <div className="mr-auto ml-auto">
-      <BrowserRouter>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer /> 
-      </BrowserRouter>
+    <div className="App flex justify-between">
+      <div className="flex-none w-1/4 p-4">
+        <MyAccount />
+      </div>
+      <div className="flex-none w-3/4 p-4">
+        <AccountDetails />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// // App.js
+// import React from 'react';
+// import MyAccount from './components/MyAccount';
+// import AccountDetails from './components/AccountDetails';
+
+// function App() {
+//   return (
+//     // <div className="mr-auto ml-auto">
+//     <div>
+//     {/* <BrowserRouter> */}
+//       <MyAccount/>
+//       {/* <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/store" element={<Store />} />
+//         <Route path="/contacts" element={<Contacts />} />
+//         <Route path="/search" element={<Search />} />
+//         <Route path="/cart" element={<Cart />} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes> */}
+//       <AccountDetails /> 
+//     {/* </BrowserRouter> */}
+//   </div>
+//     // <div className="App flex">
+//     //   <div className="flex-1 p-4">
+//     //     <MyAccount />
+//     //   </div>
+//     //   <div className="flex-2 p-4">
+//     //     <AccountDetails />
+//     //   </div>
+//     // </div>
+//   );
+// }
+
+
+// export default App;
+
+
+// // // App.js
+// // import React from 'react';
+// // import MyAccount from './components/MyAccount'; 
+// // import AccountDetails from './components/AccountDetails'; 
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <MyAccount /> {/* 使用MyAccount组件 */}
+// //       <AccountDetails/>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
