788c03c216460ce1d318bbab4fb7c0b72a5a1b4d
santten <santtusaaranen@gmail.com>
2024-09-16 10:58:42 +0300

    merging OK

--- src/components/AddToCartButton.js
+++ src/components/AddToCartButton.js
@@ -1,29 +1,7 @@
-/*/ src/components/OrderButton.js
-import React from 'react';
-import { Icon } from '@iconify/react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import './CartSummary.css';
-
-function OrderButton() {
-  const navigate = useNavigate(); // Initialize useNavigate
-
-  const handleClick = () => {
-    navigate('/payment'); // Navigate to the payment page
-  };
-
-  return (
-    <div className="orderButton" onClick={handleClick}>
-      <p>Proceed to order </p> 
-      <Icon icon="streamline:shopping-cart-3-shopping-cart-checkout" />
-    </div>
-  );
-}
-export default OrderButton;*/
-
 import React from 'react'
 import { Icon } from '@iconify/react';
 
-function OrderButton() {
+function AddToCartButton() {
   return (
     <div className="text-center px-[1rem] py-[0.5rem] bg-black w-max rounded-[99px] hover:bg-secondary">
       <p className="text-base text-white font-semibold inline">Add To Cart&ensp;</p> 
@@ -32,4 +10,4 @@ function OrderButton() {
   )
 }
 
-export default OrderButton
+export default AddToCartButton;
