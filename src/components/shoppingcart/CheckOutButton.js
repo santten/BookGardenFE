607494052681f343136e30a9e00ff5c95bd76f4a
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-09-17 16:48:46 +0300

    Update CheckOutButton

--- src/components/shoppingcart/CheckOutButton.js
+++ src/components/shoppingcart/CheckOutButton.js
@@ -5,7 +5,7 @@ function CheckOutButton() {
   return (
     <div className="text-center px-[1rem] py-[0.5rem] bg-black w-max rounded-[99px] hover:bg-secondary">
       <p className="text-base text-white font-semibold inline">Proceed to Checkout&ensp;</p> 
-      <Icon icon="material-symbols:shopping-cart" width="1.25rem" className="text-white inline"/>
+      <Icon icon="gg:check-o" width="1.25rem" className="text-white inline"/>
     </div>
   )
 }
