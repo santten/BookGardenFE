664f3dbc8bae012acfc01ed45952b4895c30134f
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-09-18 01:38:34 +0300

    Final Update

--- src/pages/productpage.js
+++ src/pages/productpage.js
@@ -19,12 +19,27 @@ function ProductPage() {
 
     // retrieve reviews 
     const reviews = reviewArray.filter((item) => item.book_id === parseInt([productID]))
+    
+    // sprint 3 
+    const handleReadMoreReviews = () => {
+        // Logic to navigate to a page or load more reviews
+        console.log("Read more reviews clicked!");
+    };
+
 
     return (
         <div className="m-auto min-h-[90vh] pb-[4rem]">
             <PathLink />
             <BookInfoCard key={"bookinfo_for_" + bookinfo.id} bookinfo={bookinfo} />
             <ReviewArea className="bg-accent" key={"reviews_for_" + bookinfo.id} reviewlist={reviews} avg_rating={bookinfo.rating} />
+            <div className="text-center mt-0 bg-grey-light">
+                <button 
+                onClick={handleReadMoreReviews} 
+                className="px-6 py-2 mb-6 text-gray-800 border border-2 border-black rounded-full hover:bg-gray-100"
+                >
+                    Read more reviews
+                </button>
+            </div>
             <Recommendations key={"recs_for_" + bookinfo.id} bookinfo={bookinfo} />
         </div>
     )
