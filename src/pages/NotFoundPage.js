fd0037450fa415d13be038fe5ccc4edd714fa158
santten <santtusaaranen@gmail.com>
2024-09-26 13:08:32 +0300

    notfound style fix

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 const NotFoundPage = () => {
   return (
     <section className='text-center flex flex-col justify-center items-center h-96'>
-      <h1 className='text-6xl font-bold mb-4'>404 Not Found</h1>
+      <h1 className='text-6xl font-title mb-4'>404 Not Found</h1>
       <p className='text-xl mb-5'>This page does not exist</p>
       <Link
         to='/'
-        className='text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4'
+        className='text-white flex flex-row items-center justify-center gap-[0.5rem] hover:bg-accent bg-primary-dark font-semibold rounded-[99px] px-[1.25rem] py-2 mt-4'
       >
-        Go Back
+        <Icon icon="tdesign:arrow-left" width="1.25rem" className="mt-[0.1rem]"></Icon>Go Back
       </Link>
     </section>
   );
