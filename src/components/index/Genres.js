f8a97e3d79acf9eb3b29d34052b77ecf05fc671e
santten <santtusaaranen@gmail.com>
2024-10-06 16:53:46 +0300

    genres style fixes

--- src/components/index/Genres.js
+++ src/components/index/Genres.js
@@ -11,58 +11,57 @@ const Genres = () => {
     <section className="w-full bg-secondary-light py-12"
       style={{
         backgroundImage: `url(${genres})`,
-        backgroundPosition: 'right',
+        backgroundPosition: 'top right',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'contain',
       }}
     >
       <div className="container">
-        <h1 className="text-center mb-8">
-          <span className="block text-black text-4xl sm:text-5xl md:text-[64px] font-title">FIND YOUR BOOK BY</span>
-          <span className="block text-secondary text-4xl sm:text-5xl md:text-[64px] font-title">GENRE</span>
+        <h1 className="text-center font-title text-5xl text-black text-4xl mb-8">
+          <span className="text-black">FIND YOUR BOOK BY</span>
+          <span className="text-secondary">&nbsp;GENRE</span>
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-
+        <div className="flex flex-col md:grid md:grid-cols-2 gap-8">
           {/* Student Books */}
           <div className="relative group">
             <img
               src={genres1}
               alt="Student Books"
-              className="object-cover rounded-[2rem] w-full h-full"
+              className="object-cover shadow-md rounded-[2rem] w-full h-full"
             />
             <div className="rounded-[2rem] absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
 
-            <Link 
+            <Link
               to="/browse/genre/books%20for%20students/page/1"
-              className="text-sm sm:text-base md:text-[1rem] font-semibold absolute top-16 sm:top-20 left-6 sm:left-8 bg-black text-white px-4 sm:px-6 py-[0.4rem] sm:py-[0.5rem] rounded-full flex items-center"
+              className="text-md font-semibold absolute top-20 left-6 sm:left-8 bg-black hover:text-primary text-white px-6 py-2 rounded-full flex items-center"
             >
-              Find more{'\u00A0'}
-              <Icon width="1.25rem" icon="tdesign:arrow-right" />
+              Find more&nbsp;
+              <Icon width="1.5rem" icon="tdesign:arrow-right" />
             </Link>
 
-            <p className="absolute top-4 sm:top-5 left-6 sm:left-8 text-black text-2xl sm:text-[2.2rem] font-title">FOR STUDENTS</p>
+            <p className="absolute top-8 left-8 text-black text-4xl font-title">FOR STUDENTS</p>
           </div>
 
-          <div className="flex flex-col gap-8">
+          <div className="h-[100%] flex flex-col gap-8 justify-between">
             {/* Children Books */}
             <div className="relative group">
               <img
                 src={genres2}
                 alt="Children Books"
-                className="object-cover h-full rounded-[2rem]"
+                className="object-cover shadow-md h-full rounded-[2rem]"
               />
               <div className="rounded-[2rem] absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
 
-              <Link 
+              <Link
                 to="/browse/genre/books%20for%20children/page/1"
-                className="text-sm sm:text-base md:text-[1rem] font-semibold absolute top-16 sm:top-20 left-6 sm:left-8 bg-black text-white px-4 sm:px-6 py-[0.4rem] sm:py-[0.5rem] rounded-full flex items-center"
+                className="text-md font-semibold absolute top-20 left-6 sm:left-8 bg-black hover:text-primary text-white px-6 py-2 rounded-full flex items-center"
               >
-                Find more{'\u00A0'}
-                <Icon width="1.25rem" icon="tdesign:arrow-right" />
+                Find more&nbsp;
+                <Icon width="1.5rem" icon="tdesign:arrow-right" />
               </Link>
 
-              <p className="absolute top-4 sm:top-5 left-6 sm:left-8 text-black text-2xl sm:text-[2.2rem] font-title">Children's books</p>
+              <p className="absolute top-8 left-8 text-black text-4xl font-title">Children's books</p>
             </div>
 
             {/* Classic Books */}
@@ -70,19 +69,19 @@ const Genres = () => {
               <img
                 src={genres3}
                 alt="Classic Books"
-                className="object-cover rounded-[2rem] w-full h-full"
+                className="object-cover shadow-md rounded-[2rem] w-full h-full"
               />
               <div className="rounded-[2rem] absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
 
-              <Link 
+              <Link
                 to="/browse/genre/classics/page/1"
-                className="text-sm sm:text-base md:text-[1rem] font-semibold absolute top-16 sm:top-20 left-6 sm:left-8 bg-black text-white px-4 sm:px-6 py-[0.4rem] sm:py-[0.5rem] rounded-full flex items-center"
+                className="text-md font-semibold absolute top-20 left-6 sm:left-8 bg-black hover:text-primary text-white px-6 py-2 rounded-full flex items-center"
               >
-                Find more{'\u00A0'}
-                <Icon width="1.25rem" icon="tdesign:arrow-right" />
+                Find more&nbsp;
+                <Icon width="1.5rem" icon="tdesign:arrow-right" />
               </Link>
 
-              <p className="absolute top-4 sm:top-5 left-6 sm:left-8 text-black text-2xl sm:text-[2.2rem] font-title">The Classics</p>
+              <p className="absolute top-8 left-8 text-black text-4xl font-title">The Classics</p>
             </div>
           </div>
         </div>
